b8349899c85b86dc4b81bff262bfb2c29303ac26
User <you@example.com>
2024-09-09 11:12:38 +0530

    Updated Enquiry API

--- bookings/admin.py
+++ bookings/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import Inquiry
+from .models import Inquiry,TestDrive
 # Register your models here.
+
 admin.site.register(Inquiry)
+admin.site.register(TestDrive)
--- bookings/urls.py
+++ bookings/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
-from .views import InquiryAPI
+from .views import InquiryAPI,TestDriveAPI
 
 
 urlpatterns = [
     
     path('inquiry',InquiryAPI.as_view()),
-    path('inquiry/<str:pk>',InquiryAPI.as_view())
+    path('inquiry/<str:pk>',InquiryAPI.as_view()),
+    
+    path('test-drive',TestDriveAPI.as_view()),
+    path('test-drive/<str:pk>',TestDriveAPI.as_view())
     
     
 ]
